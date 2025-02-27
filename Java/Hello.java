616a6eecf3affb0e034e54573c6692115379123e
preetimagadum <preetimagadum2@gmail.com>
2025-02-27 12:20:11 +0530

    python

--- Java/Hello.java
+++
@@ -1,5 +0,0 @@
-class Text{
-    public static void main(String[] args){
-        System.out.println("Hello");
-    }
-}
---
+++ python pr practice/stringE1.py
@@ -0,0 +1,4 @@
+string_number="128"
+float_number=float(string_number)
+print(float_number)
+print(type(float_number))
